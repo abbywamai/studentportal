157f3fdfb4ba87de33930cd05ef91ccb8b59e002
Abigail Wamai <abigail.wamai@student.moringaschool.com>
2023-10-06 08:51:06 +0300

    codes

--- client/studentportal/src/StudentAction.jsx
+++ client/studentportal/src/StudentAction.jsx
@@ -1,48 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-const StudentActions = ({ userType }) => {
+const StudentActions = () => {
   const [studentName, setStudentName] = useState('');
   const [myGrade, setMyGrade] = useState('');
 
   const checkGrade = async () => {
     try {
-      const response = await axios.get(`/api/get-grade?studentName=${studentName}`);
+      const response = await axios.get(`/student/grades?studentName=${studentName}`);
       setMyGrade(response.data.grade);
     } catch (error) {
       console.error(error);
     }
   };
 
-  useEffect(() => {
-    if (userType === 'student') {
-      checkGrade();
-    }
-  }, [userType]);
-
   return (
-    <div className="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
-      <h1 className="text-xl font-bold mb-4">Student Actions</h1>
-      {userType === 'student' && (
-        <>
-          <div className="mb-4">
-            <input
-              type="text"
-              placeholder="Enter student's full name"
-              value={studentName}
-              onChange={(e) => setStudentName(e.target.value)}
-              className="border p-2 w-full rounded-md"
-            />
-          </div>
-          <button
-            onClick={checkGrade}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Check My Grade
-          </button>
-          <p className="mt-4">{`Name: ${studentName} Grade: ${myGrade}`}</p>
-        </>
-      )}
+    <div className="max-w-md mx-auto my-4 p-6 bg-white rounded shadow-lg">
+      <h2 className="text-2xl font-bold mb-4">Student Actions</h2>
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Enter student's full name"
+          value={studentName}
+          onChange={(e) => setStudentName(e.target.value)}
+          className="w-full px-4 py-2 border rounded"
+        />
+      </div>
+      <button onClick={checkGrade} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Check My Grade
+      </button>
+      <p className="mt-4">{`Name: ${studentName} Grade: ${myGrade}`}</p>
     </div>
   );
 };
--- client/studentportal/src/TeacherAction.jsx
+++ client/studentportal/src/TeacherAction.jsx
@@ -1,53 +1,62 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const TeacherActions = ({ userType }) => {
+const TeacherActions = () => {
   const [studentId, setStudentId] = useState('');
-  const [newGrade, setNewGrade] = useState('');
+  const [courseId, setCourseId] = useState('');
+  const [gradeValue, setGradeValue] = useState('');
 
-  const handleGradeChange = async () => {
+  const handlePostGrade = async () => {
     try {
-      await axios.post('/api/change-grade', { studentId, newGrade });
-      alert('Grade changed');
+      const response = await axios.post('http://localhost:5000/teacher/postgrades', {
+        student_id: studentId,
+        course_id: courseId,
+        grade_value: gradeValue
+      });
+      alert('Grade posted successfully');
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <div className="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
-      <h1 className="text-xl font-bold mb-4">Teacher Actions</h1>
-      {userType === 'teacher' && (
-        <>
-          <div className="mb-4">
-            <input
-              type="text"
-              placeholder="Enter student's ID"
-              value={studentId}
-              onChange={(e) => setStudentId(e.target.value)}
-              className="border p-2 w-full rounded-md"
-            />
-          </div>
-          <div className="mb-4">
-            <input
-              type="text"
-              placeholder="Enter new grade"
-              value={newGrade}
-              onChange={(e) => setNewGrade(e.target.value)}
-              className="border p-2 w-full rounded-md"
-            />
-          </div>
-          <button
-            onClick={handleGradeChange}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Change Grade
-          </button>
-        </>
-      )}
+    <div className="max-w-md mx-auto my-4 p-6 bg-white rounded shadow-lg">
+      <h2 className="text-2xl font-bold mb-4">Teacher Actions</h2>
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Student ID"
+          value={studentId}
+          onChange={(e) => setStudentId(e.target.value)}
+          className="w-full px-4 py-2 border rounded"
+        />
+      </div>
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Course ID"
+          value={courseId}
+          onChange={(e) => setCourseId(e.target.value)}
+          className="w-full px-4 py-2 border rounded"
+        />
+      </div>
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Grade Value"
+          value={gradeValue}
+          onChange={(e) => setGradeValue(e.target.value)}
+          className="w-full px-4 py-2 border rounded"
+        />
+      </div>
+      <button
+        onClick={handlePostGrade}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Post Grade
+      </button>
     </div>
   );
 };
 
 export default TeacherActions;
-
